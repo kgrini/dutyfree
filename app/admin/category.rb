0b06dbfe39c98d422a9394158b4d27d5ca9b1a36
kgrini <kirill.grini@gmail.com>
2014-01-09 00:07:49 +0200

    was restructured tables in admin + sass modify

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -8,10 +8,28 @@ ActiveAdmin.register Category do
   sidebar "Category Products", only: [:show, :edit] do
     ul do
       li link_to("New product", new_admin_category_product_path(category.id))
-      li link_to("All products", admin_categories_path)
+      li link_to("All products", admin_category_products_path(category.id))
     end
   end
 
+  index do
+    column :id
+    column :name
+    column :created_at
+    column :products do |category|
+      ul do
+        li link_to("All products", admin_category_products_path(category))
+        li link_to("Create Product", new_admin_category_product_path(category))
+      end
+    end
+    column :functions do |category|
+      ul do
+        li link_to("View", admin_category_path(category))
+        li link_to("Edit", edit_admin_category_path(category.id))
+        li link_to("Delete", admin_category_path(category), method: :delete, :confirm => "Are you Sure?")
+      end
+    end
+  end
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
