aca7b12206cc1653474bfd2d88921cd65dd1b57f
kgrini <kirill.grin2@gmail.com>
2014-01-17 15:16:27 +0200

    fixed bug related with carts_id

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ActiveRecord::Base
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
-      #item.card_id = nil
+      item.cart_id = nil
       line_items << item
     end
   end
