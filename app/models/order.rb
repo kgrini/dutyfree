3dd4253f001dc82b5d706c250a121c444398f31f
kgrini <kirill.grin2@gmail.com>
2014-01-20 11:23:14 +0200

    added order form validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,13 @@ class Order < ActiveRecord::Base
   has_many :carts
   has_many :line_items, :dependent => :destroy
 
+  validates :name, :presence => true, :length => {:minimum => 4, :maximum => 20}
+  validates :address, :presence => true, :length => {:minimum => 4, :maximum => 20}
+  validates :phone, :presence => true, :length => {:minimum => 4, :maximum => 20}, :numericality => { :only_integer => true}
+
+  VALIDATE_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, :presence => true, :format => { :with => VALIDATE_EMAIL}
+
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
