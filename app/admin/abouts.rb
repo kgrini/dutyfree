e5ebe8cc1f3e80f3803df1b32bcadd58165e14e8
Kirill <kirill@kirill-NF720D-A2G.(none)>
2014-01-29 19:34:22 +0200

    added text editor at admin site

--- app/admin/abouts.rb
+++ app/admin/abouts.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register About do
 
   permit_params :content
+
+  form do |f|
+
+    f.inputs do
+      f.input :content, :as => :html_editor
+    end
+
+    f.actions
+  end
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
