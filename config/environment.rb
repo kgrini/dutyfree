f0cb003991b77c02072b5d7cd4d9cd3def59d652
Kirill <kirill@kirill-NF720D-A2G.(none)>
2014-01-29 20:17:33 +0200

    implemented sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Dutyfree::Application.initialize!
 
+
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app21544868@heroku.com'],
-    :password       => ENV['gchn7hwt'],
+    :user_name      => 'app21544868@heroku.com',
+    :password       => 'gchn7hwt',
     :domain         => 'dutyfree-kgrini.heroku.com',
     :enable_starttls_auto => true
 }
