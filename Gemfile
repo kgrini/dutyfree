b290b9cc547510b7faa239ffef230999f51b14c2
kgrini <kirill.grin2@gmail.com>
2014-01-29 16:36:31 +0200

    added text editor on admin site

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'coffee-rails', '~> 4.0.0'
 
 gem 'haml'
 
+gem 'active_admin_editor'
+
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,2 +1,12 @@
 class AboutsController < ApplicationController
+
+  before_filter :current_admin_user, :only => [:new, :create, :edit, :destroy, :update]
+  before_action :all_categories, :only => [:new, :index, :edit, :show, :create]
+  before_action :show_products, :only => [:index]
+
+  def index
+    @about_message = About.first
+    @cart = current_cart
+  end
+
 end
